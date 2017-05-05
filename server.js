cbdd4224e958bcaa15066ce3618162e35def48b0
Yurika Jacobsson <nakiruy@gmail.com>
2017-05-04 23:34:48 -0700

    Added app.json again.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const db = require('./db');
 const app = express();
+const AppJSON = require("app.json")
 
 var port = process.env.PORT;
 
