43b68e0c4da6af2716e2c22d139e38d7a687f769
Yurika Jacobsson <nakiruy@gmail.com>
2017-05-05 00:07:30 -0700

    Deleted AppJSON from server.js.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const db = require('./db');
 const app = express();
-const AppJSON = require("app.json")
 
 var port = process.env.PORT;
 
